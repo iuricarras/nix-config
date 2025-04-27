318a45314943f571936b9e701882d417502c3d68
iuricarras <iuri.carrasqueiro@gmail.com>
2025-04-27 17:52:45 +0100

    Add NixOS configuration for host 'rufus' including hardware setup

---
+++ home/iuricarras/rufus.nix
@@ -0,0 +1,43 @@
+{ ... }:
+{
+  imports = [
+    #
+    # ========== Required Configs ==========
+    #
+    common/core
+
+    #
+    # ========== Host-specific Optional Configs ==========
+    #
+    common/optional/gaming
+    common/optional/tools
+
+    common/optional/gns3.nix
+    common/optional/sops.nix
+  ];
+
+
+   #
+  # ========== Host-specific Monitor Spec ==========
+  #
+  # This uses the nix-config/modules/home/montiors.nix module which defaults to enabled.
+  # Your nix-config/home-manger/<user>/common/optional/desktops/foo.nix WM config should parse and apply these values to it's monitor settings
+  # If on hyprland, use `hyprctl monitors` to get monitor info.
+  # https://wiki.hyprland.org/Configuring/Monitors/
+  #    ------
+  # | Internal |
+  # | Display  |
+  #    ------
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1080;
+      refreshRate = 144;
+      primary = true;
+      #vrr = 1;
+    }
+  ];
+
+
+}
---
+++ hosts/nixos/rufus/default.nix
@@ -0,0 +1,105 @@
+{
+  inputs,
+  lib,
+  ...
+}: let
+in {
+  imports = [
+    #
+    # ========== Hardware ==========
+    #
+    ./hardware-configuration.nix
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-nvidia-prime
+    inputs.hardware.nixosModules.common-gpu-nvidia-turing
+    inputs.hardware.nixosModules.common-pc-laptop
+    inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.common-pc-hdd
+
+    inputs.nurpkgs.modules.nixos.default
+    #
+    # ========== Disk Layout ==========
+    #
+    #inputs.disko.nixosModules.disko
+
+    #
+    # ========== Misc Inputs ==========
+    #
+
+    (map lib.custom.relativeToRoot [
+      #
+      # ========== Required Configs ==========
+      #
+      "hosts/common/core"
+      "hosts/common/disks/hdd.nix"
+      "hosts/common/disks/home.nix"
+      #
+      # ========== Non-Primary Users to Create ==========
+      #
+
+      #
+      # ========== Optional Configs ==========
+      #
+      "hosts/common/optional/bootloader/grub.nix"
+      "hosts/common/optional/desktopEnvironment/gnome.nix"
+      "hosts/common/optional/virtualization/virtualbox.nix"
+      "hosts/common/optional/virtualization/libvirt.nix"
+      "hosts/common/optional/virtualization/vmware.nix"
+      "hosts/common/optional/audio.nix"
+      "hosts/common/optional/gaming.nix"
+      "hosts/common/optional/plymouth.nix"
+      "hosts/common/optional/swap.nix"
+    ])
+  ];
+
+  #
+  # ========== Host Specification ==========
+  #
+
+  hostSpec = {
+    hostName = "rufus";
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    enableIPv6 = false;
+  };
+
+  boot.initrd = {
+    systemd.enable = true;
+  };
+
+  # Nvidia GPU
+  hardware = {
+    nvidia = {
+      modesetting.enable = lib.mkDefault true;
+      powerManagement.enable = lib.mkDefault true;
+
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+  # Conservation Mode - Lenovo
+  systemd.tmpfiles.settings = {
+    "ideapad-set-conservation-mode" = {
+      "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode" = {
+        "f+" = {
+          group = "root";
+          user = "root";
+          mode = "0644";
+          argument = "1";
+        };
+      };
+    };
+  };
+
+  # Undervolt - CPU
+  services.undervolt.enable = true;
+  services.undervolt.coreOffset = -130;
+
+  # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "24.11";
+}
